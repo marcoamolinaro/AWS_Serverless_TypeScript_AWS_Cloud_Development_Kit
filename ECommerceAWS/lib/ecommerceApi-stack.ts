413d1758264fa0e3ea4360e15de8690880c6aaad
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-04-14 17:02:57 -0300

    Integra o API Gateway e Lambda de get de produtos

--- ECommerceAWS/lib/ecommerceApi-stack.ts
+++ ECommerceAWS/lib/ecommerceApi-stack.ts
@@ -4,14 +4,24 @@ import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs"
 import * as apigateway from "aws-cdk-lib/aws-apigateway"
 import * as cwlogs from "aws-cdk-lib/aws-logs"
 
+interface ECommerceApiStackProps extends cdk.StackProps {
+    productsFetchHandler: lambdaNodeJs.NodejsFunction
+}
+
 export class ECommerceApiStack extends cdk.Stack {
     
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
         super(scope, id, props)
 
         const api = new apigateway.RestApi(this, "ECommerceApi", {
             restApiName: "ECommerceApi"
         })
+
+        const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+
+        // "/products"
+        const productsResource = api.root.addResource("products")
+        productsResource.addMethod("GET", productsFetchIntegration)
     }
 
 }
